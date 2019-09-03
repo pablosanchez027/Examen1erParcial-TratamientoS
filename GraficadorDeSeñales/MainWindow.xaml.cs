612312092bdbbffa148b2c113316307f7de21eb0
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-03 10:27:21 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorDeSeñales/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraficadorDeSeñales
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void BtnGraficar_Click(object sender, RoutedEventArgs e)
+        {
+            double amplitud = double.Parse(txtAmplitud.Text);
+            double fase = double.Parse(txtFase.Text);
+            double frecuencia = double.Parse(txtFrecuencia.Text);
+            double tiempoinicial = double.Parse(txtTiempoInicial.Text);
+            double tiempofinal = double.Parse(txtTiempoFinal.Text);
+            double frecuenciamuestreo = double.Parse(txtFrecuenciaMuestreo.Text);
+
+            //SeñalSenoidal señal = new SeñalSenoidal(amplitud, fase, frecuencia);
+            //SeñalParabolica señal = new SeñalParabolica();
+            //FuncionSigno señal = new FuncionSigno();
+            Triangular señal = new Triangular();
+
+            double periodioMuestreo = 1.0 / frecuenciamuestreo;
+            double amplitudMaxima = 0.0;
+
+            plnGrafica.Points.Clear();
+
+            for(double i = tiempoinicial; i <= tiempofinal; i += periodioMuestreo)
+            {
+                double valorMuestra = señal.evaluar(i);
+                if (Math.Abs(valorMuestra) > amplitudMaxima)
+                {
+                    amplitudMaxima = Math.Abs(valorMuestra);
+                }
+                Muestra muestra = new Muestra(i, señal.evaluar(i));
+                señal.Muestras.Add(muestra);
+            }
+
+            foreach (Muestra muestra in señal.Muestras)
+            {
+                plnGrafica.Points.Add(adaptarCoordenadas(muestra.X, muestra.Y, tiempoinicial, amplitudMaxima));
+            }
+
+            lblLimiteSuperior.Text = amplitudMaxima.ToString();
+            lblLmiteInferior.Text = "-" + amplitudMaxima.ToString();
+
+            plnEjeX.Points.Clear();
+            plnEjeX.Points.Add(adaptarCoordenadas(tiempoinicial, 0.0, tiempoinicial, amplitudMaxima));
+            plnEjeX.Points.Add(adaptarCoordenadas(tiempofinal, 0.0, tiempoinicial, amplitudMaxima));
+
+            plnEjeY.Points.Clear();
+            plnEjeY.Points.Add(adaptarCoordenadas(0.0, amplitudMaxima, tiempoinicial, amplitudMaxima));
+            plnEjeY.Points.Add(adaptarCoordenadas(0.0, -amplitudMaxima, tiempoinicial, amplitudMaxima));
+
+        }
+        public Point adaptarCoordenadas(double x, double y, double tiempoinicial, double amplitudMaxima)
+        {
+            return new Point((x - tiempoinicial) * srcGrafica.Width, (-1 * (y * (((srcGrafica.Height / 2.0) - 25) / amplitudMaxima))) + (srcGrafica.Height / 2));
+        }
+    }
+}
---
+++ GraficadorDeSeñales/Triangular.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorDeSeñales
+{
+    class Triangular
+    {
+        public List<Muestra> Muestras { get; set; }
+
+        public Triangular()
+        {
+            Muestras = new List<Muestra>();
+        }
+
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+            if (Math.Abs(tiempo) < 1)
+            {
+                resultado = 1 - Math.Abs(tiempo);
+            }
+            else if (Math.Abs(tiempo) >= 1)
+            {
+                resultado = 0.0;
+            }
+            else
+            {
+                resultado = 0.0;
+            }
+            return resultado;
+        }
+    }
+}
